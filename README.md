7d7da44a39afee60cd6e88be8b25718bb5d97977
brockco1 <brockco@iu.edu>
2019-09-25 23:15:12 -0500

    Updated main 1 2 3

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 # E05a-Animation
 
-We will begin to explore keyboard and mouse control in a Python Arcade program.
-
-When you are done, main1.py should contain the code on [http://arcade.academy/examples/move_mouse.html#move-mouse](http://arcade.academy/examples/move_mouse.html#move-mouse). Play with the code until you feel like you understand what is going on. Comment any uncommented line; feel free to ask for help!
-
-main2.py should contain the code on [http://arcade.academy/examples/move_keyboard.html#move-keyboard](http://arcade.academy/examples/move_keyboard.html#move-keyboard). Play with the code until you feel like you understand what is going on. Comment any uncommented line; feel free to ask for help!
-
-For extra credit, you could create a main3.py that contains the code on [http://arcade.academy/examples/sprite_move_animation.html#sprite-move-animation](http://arcade.academy/examples/sprite_move_animation.html#sprite-move-animation). Download sprite assets for the player and coins and figure out how to make your character walk left and right.
+In this assignment I begin to explore keyboard and mouse control in a Python Arcade program.
+I added comments to main1 and 2 and ran the code in the terminal then I tryed to do the extra credit assignment in main3
---
+++ main3.py
@@ -0,0 +1,152 @@
+"""
+import arcade
+import random
+import os
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Move with a Sprite Animation Example"
+
+COIN_SCALE = 0.5
+COIN_COUNT = 50
+
+MOVEMENT_SPEED = 5
+
+class MyGame(arcade.Window):
+
+    def __init__(self, width, height, title):
+        
+        super().__init__(width, height, title)
+
+        # Set the working directory (where we expect to find files) to the same
+        # directory this .py file is in. You can leave this out of your own
+        # code, but it is needed to easily run the examples using "python -m"
+        # as mentioned at the top of this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
+        
+
+        # Sprite lists
+        self.player_list = none
+        self.coin_list = none
+
+        # Set up the player
+        self.score = 0
+        self.player = Soldier
+
+    def setup(self):
+        self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        # Set up the player
+        self.score = 0
+        self.player = arcade.AnimatedWalkingSprite()
+
+        character_scale = 0.75
+        self.player.stand_right_textures = []
+        self.player.stand_right_textures.append(arcade.load_texture("PNG/Soldier/Poses/soldier_stand.png",
+                                                                    scale=character_scale))
+        self.player.stand_left_textures = []
+        self.player.stand_left_textures.append(arcade.load_texture("PNG/Soldier/Poses/soldier_stand.png",
+                                                                   scale=character_scale, mirrored=True))
+
+        self.player.walk_right_textures = []
+
+        self.player.walk_right_textures.append(arcade.load_texture("PNG/Soldier/Poses/soldier_walk1.png",
+                                                                   scale=character_scale))
+        self.player.walk_right_textures.append(arcade.load_texture("PNG/Soldier/Poses/soldier_walk2.png",
+                                                                   scale=character_scale))
+        
+
+        self.player.walk_left_textures = []
+
+        self.player.walk_left_textures.append(arcade.load_texture("PNG/Soldier/Poses/soldier_walk1.png",
+                                                                  scale=character_scale, mirrored=True))
+        self.player.walk_left_textures.append(arcade.load_texture("PNG/Soldier/Poses/soldier_walk2.png",
+                                                                  scale=character_scale, mirrored=True))
+       
+
+        self.player.texture_change_distance = 20
+
+        self.player.center_x = SCREEN_WIDTH // 2
+        self.player.center_y = SCREEN_HEIGHT // 2
+        self.player.scale = 0.8
+
+        self.player_list.append(self.player)
+
+        for i in range(COIN_COUNT):
+            coin = arcade.AnimatedTimeSprite(scale=0.5)
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(SCREEN_HEIGHT)
+
+            coin.textures = []
+            coin.textures.append(arcade.load_texture("Gold/gold_coin_round_diamond_2@2x.png", scale=COIN_SCALE))
+            coin.cur_texture_index = random.randrange(len(coin.textures))
+
+            self.coin_list.append(coin)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def on_draw(self):
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+        self.soldier.draw()
+        # Draw all the sprites.
+        self.coin_list.draw()
+        self.player_list.draw()
+
+        # Put the text on the screen.
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+
+    def on_key_press(self, key, modifiers):
+        
+        #Called whenever a key is pressed.
+        
+        if key == arcade.key.UP:
+            self.player.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        
+        #Called when the user releases a key.
+        
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player.change_x = 0
+
+    def update(self, delta_time):
+         #Movement and game logic 
+
+        self.coin_list.update()
+        self.coin_list.update_animation()
+        self.player_list.update()
+        self.player_list.update_animation()
+
+        # Generate a list of all sprites that collided with the player.
+        hit_list = arcade.check_for_collision_with_list(self.player, self.coin_list)
+
+        # Loop through each colliding sprite, remove it, and add to the score.
+        for coin in hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+
+def main():
+    # Main method 
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+
+    if (__name__ == "__main__"):    
+    main()
+
